af79231c43e18107639c8ecc1adac2ce279f2825
Armen Vartan <vartan.armen@gmail.com>
2016-11-11 11:43:45 -0500

    moved a bunch of stuff from skeleton to here

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+'use strict';
 var path = require("path");
-
+var _ = require('lodash')
 var context = {
     controllers: {},
+    dbs: {}, // NOTE must be set before controllers
     //environment variables
     env: {},
+    errors: require(path.normalize(__dirname + "/lib/errors.js")),
     helpers: require(path.normalize(__dirname + "/lib/helpers.js")),
     //environment mode, development, staging, production...
     mode: "",
     //globals setter and getter
     globals: require(path.normalize(__dirname + "/lib/globals.js")),
+    logger: require(path.normalize(__dirname + "/lib/logger.js")),
     //middleware
     middleware: {},
     //root uri for all routes in schema
+    requestHandler: require(path.normalize(__dirname + "/lib/requestHandler.js")),
     rootUri: '',
     //routes declaration
     routes: {},
@@ -27,10 +32,12 @@ var context = {
 //event callbacks
 context.events = require(path.normalize(__dirname + "/lib/events.js")).call(context);
 
-//service discovery interface
-context.discovery = function(interface, config) { return require(path.normalize(__dirname + "/lib/discovery.js")).call(context, interface, config) }
+//service discovery serviceInterface
+context.discovery = function (serviceInterface, config) {
+    return require(path.normalize(__dirname + "/lib/discovery.js")).call(context, serviceInterface, config)
+}
 
  //init function
-context.start = function(info){ require(path.normalize(__dirname + "/lib/init.js")).call(context, info) }
+context.start = function (info) { require(path.normalize(__dirname + "/lib/init.js")).call(context, info) }
 
 module.exports = context;
